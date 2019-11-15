31bf8a2c8cee6bfdee16b0aca24ca4011c75aff3
Jacques W <jacqueswickersheim@orange.fr>
2019-11-15 18:09:13 +0100

    Test a faire + event listener

---
+++ test formulaire js/WebContent/js/index.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+	
+	let name = 'name'
+		
+	name.addEventListener('focus', name = function(){
+		console.log('test focus')
+	})
+}//Fin window.onload
+
+function name(){
+	console.log('TestNom en cliquant')
+}
